9ab8004eb36cdcf6c8b3c830c34fabee392d082d
Maxime Desmet Vanden Stock <maximedesmet@yahoo.com>
2025-06-25 14:52:23 +0200

    Add asynchronous JCDecaux API client

--- libs/jcdecauxclient/__init__.py
+++ libs/jcdecauxclient/__init__.py
@@ -1,8 +1,10 @@
 from .client import JCDecauxClient
+from .async_client import JCDecauxAsyncClient
 from .models import Contract, Park, Position, Station, Stands
 
 __all__ = [
     "JCDecauxClient",
+    "JCDecauxAsyncClient",
     "Contract",
     "Park",
     "Position",
