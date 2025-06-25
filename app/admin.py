b41bf38c3316ba0137bdd5a48f15962522a62519
Maxime Desmet Vanden Stock <maximedesmet@yahoo.com>
2025-06-25 13:54:40 +0200

    Add Station model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
+"""Admin configuration for app models."""
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Station
+
+
+@admin.register(Station)
+class StationAdmin(admin.ModelAdmin):
+    list_display = ["contract_name", "number", "name", "status", "last_update"]
+    list_filter = ["contract_name", "status", "banking", "bonus"]
+    search_fields = ["name", "address", "number"]
