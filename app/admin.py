556d5ae43102841b357a66c25977c41edf42d20e
Maxime Desmet Vanden Stock <maximedesmet@yahoo.com>
2025-06-26 09:59:16 +0200

    Refactor models to match API structure

--- app/admin.py
+++ app/admin.py
@@ -2,12 +2,29 @@
 
 from django.contrib import admin
 
-from .models import Station, Contract
+from .models import Contract, Position, Stands, Station
 
 
 @admin.register(Contract)
 class ContractAdmin(admin.ModelAdmin):
     list_display = ["name", "commercial_name", "country_code"]
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ["latitude", "longitude"]
+
+
+@admin.register(Stands)
+class StandsAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "bikes",
+        "stands",
+        "mechanical_bikes",
+        "electrical_bikes",
+        "capacity",
+    ]
 
 
 @admin.register(Station)
